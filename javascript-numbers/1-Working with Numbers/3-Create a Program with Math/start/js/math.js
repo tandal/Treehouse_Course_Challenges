63d86df0e8e6438ce4fca887954b77a639b8b607
tylerandal <tyler.andal@gmail.com>
2021-06-10 10:55:43 -0500

    Finished the match project

---
+++ javascript-numbers/1-Working with Numbers/3-Create a Program with Math/start/js/math.js
@@ -0,0 +1,14 @@
+const secsPerMin = 60;
+const minsPerHour = 60;
+const hoursPerDay = 24;
+const daysPerWeek = 7;
+const weeksPerYear = 52;
+const daysPerYear = 365;
+let currentAge = prompt('How old are you?');
+
+
+const secondsPerDay = secsPerMin * minsPerHour * hoursPerDay;
+console.log(`There are ${secondsPerDay} seconds in a day.`);
+
+const yearsAliveInSeconds = secondsPerDay * daysPerYear *currentAge;
+console.log(`I've been alive for more than ${yearsAliveInSeconds} seconds!`)
