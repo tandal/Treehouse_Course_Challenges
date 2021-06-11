c64fac09b6275635ceb574db044f0f33e36598fd
tylerandal <tyler.andal@gmail.com>
2021-06-10 20:58:12 -0500

    Completed javascript numbers

---
+++ javascript-numbers/1-Working with Numbers/3-Create a Program with Math/start/js/random.js
@@ -0,0 +1,3 @@
+const dieRoll = Math.floor( Math.random() * 6 ) + 1;
+console.log(`You rolled a ${dieRoll}.`);
+
--- javascript-numbers/2-The Math Object/3-Random Number Challenge/start/js/random.js
+++ javascript-numbers/2-The Math Object/3-Random Number Challenge/start/js/random.js
@@ -1,11 +1,17 @@
 // Collect input from a user
-
+let number = prompt('Please enter a number');
 
 // Convert the input to a number
+let highNumber = parseInt(number);
 
-
+if (highNumber) {
 // Use Math.random() and the user's number to generate a random number
-
+let randomNumber = Math.floor( Math.random( ) * highNumber ) + 1 ;
 
 // Create a message displaying the random number
-
+document.querySelector('main').innerHTML = `<p>${randomNumber} is a random number between 1 and ${highNumber}.</p>`;
+} else if (highNumber === 0){
+    document.querySelector('main').innerHTML = '<p>Please pick a number other than 0.</p>';
+} else {
+    document.querySelector('main').innerHTML = '<p>Please enter a number.</p>';
+}
