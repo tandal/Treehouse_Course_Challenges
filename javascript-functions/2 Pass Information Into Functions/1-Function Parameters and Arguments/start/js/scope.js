85959c901e5f6846df2555dd045949dad635fe46
tylerandal <tyler.andal@gmail.com>
2021-06-11 12:46:15 -0500

    section 2 complete

--- javascript-functions/2 Pass Information Into Functions/1-Function Parameters and Arguments/start/js/scope.js
+++ javascript-functions/2 Pass Information Into Functions/1-Function Parameters and Arguments/start/js/scope.js
@@ -1,4 +1,16 @@
+// Global scope
+const person = 'Lee';
+
 function greeting() {
   let person = 'Meg';
   alert(`Hi, ${person}!`);
 }
+
+function greeting2() {
+  let person = 'Robert';
+  alert(`Good morning, ${person}!`);
+}
+
+greeting();
+alert(`Hi, ${person}`);
+greeting2();
