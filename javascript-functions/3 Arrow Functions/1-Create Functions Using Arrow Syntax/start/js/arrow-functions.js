e1485515cebca121d8ae396b10d5653f9a2e208e
tylerandal <tyler.andal@gmail.com>
2021-06-12 07:55:52 -0500

    saving course work

--- javascript-functions/3 Arrow Functions/1-Create Functions Using Arrow Syntax/start/js/arrow-functions.js
+++ javascript-functions/3 Arrow Functions/1-Create Functions Using Arrow Syntax/start/js/arrow-functions.js
@@ -1,4 +1,21 @@
-function getRandomNumber() {
-  const randomNumber = Math.floor( Math.random() * 6 ) + 1;
+const getRandomNumber = (upper) => {
+  const randomNumber = Math.floor( Math.random() * upper ) + 1;
   return randomNumber;
+};
+
+
+/*
+Homework
+Write the function below as an arrow function.
+
+function getArea(width, length, unit) {
+  const area = width * length;
+  return `${area} ${unit}`;
+}
+*/
+
+//Adding a more robust return message.
+const getArea = (width, length, unit) => {
+  const area = width * length;
+  return `The area = ${area}, and the unit = ${unit}`;
 }
