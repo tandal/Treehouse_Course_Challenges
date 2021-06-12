9bfeb6f63cc2eabf00137b484e69ed6577a41c33
tylerandal <tyler.andal@gmail.com>
2021-06-12 09:02:08 -0500

    saving course work

--- javascript-functions/3 Arrow Functions/5-Function Challenge/start/js/random.js
+++ javascript-functions/3 Arrow Functions/5-Function Challenge/start/js/random.js
@@ -6,8 +6,16 @@
  * @return {number} The random number value.
  */
 
-Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+function randomNumber (lower = 1, upper = 100) {
+    if ( isNaN(lower) || isNaN(upper) ) {
+        throw Error('Both arguments must be numbers.');
+    }
+    return Math.floor(Math.random() * (upper - 1 + 1)) + lower;
+}
 
 // Call the function and pass it different values
 
-
+console.log( randomNumber(1,6) );
+console.log( randomNumber(50, 100) );
+console.log( randomNumber(40, 60) );
+console.log( randomNumber(1, 'three hundred') );
