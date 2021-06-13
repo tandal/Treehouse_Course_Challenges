24b8d19eb9536a978b7d77f6c911389e09f31e65
tylerandal <tyler.andal@gmail.com>
2021-06-13 11:31:42 -0500

    saving course work

--- javascript-loops/2 - Working with for Loops/2 - Dynamically Display HTML with a Loop/start/js/break.js
+++ javascript-loops/2 - Working with for Loops/2 - Dynamically Display HTML with a Loop/start/js/break.js
@@ -5,6 +5,7 @@ for ( let i = 5; i >= 1; i-- ) {
   let guess = prompt(`Enter the secret word. You have ${i} tries.`);
   if ( guess === secretWord ) {
     message = 'Welcome to the secret loop world!';
+    break; //immediately terminate the loop
   }
 }
 
